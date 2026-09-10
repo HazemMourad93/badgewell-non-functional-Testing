d5c6449f307f426f3fe26ad19e6d8b8e21815b98
HazemMourad93 <hazem.farag@badgewell.com>
2026-09-10 14:44:51 +0300

    THL-PRF-CONCURREENT-ACTIONS

--- src/test/java/io/THLInterviewSceanrios/THLInterviewConcurrencySimulation.java
+++ src/test/java/io/THLInterviewSceanrios/THLInterviewConcurrencySimulation.java
@@ -13,11 +13,11 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 public class THLInterviewConcurrencySimulation extends Simulation {
     String admintoken1 = ConfigReader.properties.getProperty("admintoken1");
     ScenarioBuilder scn = perf03ConcurrentInterviewOperations(admintoken1,
-            "66252f12e0313ea0b127ef7a", "6aa274cc24d854265a58e93e");
+            "66252f12e0313ea0b127ef7a", "6aa2969c34028cd21149e549");
     {
         setUp(scn
                 .injectOpen(rampUsers(50)
-                        .during(Duration.ofSeconds(30))))
+                        .during(Duration.ofSeconds(60))))
                 .protocols(HttpConfig.baseConfig())
                 .assertions(global().successfulRequests().percent().gt(95.0),
                         global().responseTime().percentile3().lt(5000),
